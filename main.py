896cccc1c6125eda14fd28f27a4a60f8de5b4776
Suntoria <sicarius236@gmail.com>
2021-02-06 02:44:30 +0000

    Changed Total and Cost function returns to Embed version, still need to insert corresponding emoji/picture

--- main.py
+++ main.py
@@ -49,13 +49,35 @@ async def upgrade_cost(ctx, current_level: int = 1, target_level: int = 10000):
         print(f"Error: {error_code_table[perimeter_status]}")
         await ctx.send(f"{error_code_table[perimeter_status]}")
     else:
-        await ctx.send(
-            f"Hero | Leader | Tower:\t{upgrade_cost_diff(current_level, target_level):,}\tgold\n"
-            +
-            f"Castle:\t\t\t\t\t\t\t\t{upgrade_castle_diff(current_level, target_level):,}\tgold\n"
-            +
-            f"Tower-Archer (TA):\t\t{upgrade_TA_diff(current_level, target_level):,}\tgold"
+        embed = discord.Embed(
+            title="Upgrade Cost",
+            description=
+            f"Value difference in gold from level {current_level:,} to level {target_level:,}",
+            url='https://repl.it/@Suntoria/sun-bot#main.py',
+            color=0xFF5733)  #,color=Hex code
+        embed.set_author(
+            name=ctx.author.display_name,
+            url=
+            'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO',
+            icon_url=ctx.author.avatar_url)
+        embed.set_thumbnail(
+            url=
+            'https://static.wikia.nocookie.net/grow-castle/images/1/1f/Gold_Coin.png/revision/latest?cb=20180508232731'
         )
+        embed.add_field(
+            name='Hero | Leader | Tower',
+            value=f'{upgrade_cost_diff(current_level, target_level):,}\tgold',
+            inline=False)
+        embed.add_field(
+            name='Castle',
+            value=f'{upgrade_castle_diff(current_level, target_level):,}\tgold',
+            inline=False)
+        embed.add_field(
+            name='Tower-Archer',
+            value=f'{upgrade_TA_diff(current_level, target_level):,}\tgold',
+            inline=False)
+
+        await ctx.send(embed=embed)
 
 
 @upgrade.command(
@@ -63,44 +85,30 @@ async def upgrade_cost(ctx, current_level: int = 1, target_level: int = 10000):
     help=
     '[target lvl] | Get the cost in gold to get from level 1 to target-level')
 async def total_cost(ctx, target_level: int = 10000):
-    await ctx.send(
-        f"Hero | Leader | Tower:\t{upgrade_cost_total( target_level):,}\tgold\n"
-        +
-        f"Castle:\t\t\t\t\t\t\t\t{upgrade_castle_total( target_level):,}\tgold\n"
-        + f"Tower-Archer (TA):\t\t{upgrade_TA_total( target_level):,}\tgold")
-
-
-@bot.command(name='embed', help='testing Embedding')
-async def embed(ctx):
-    embed = discord.Embed(title="Your title here",
-                          description="Your desc here",
-                          url='https://repl.it/@Suntoria/sun-bot#main.py',
-                          color=0xFF5733)  #,color=Hex code
-    # embed.set_author(
-    #     name='Suntoria',
-    #     url='https://repl.it/@Suntoria',
-    #     icon_url=
-    #     'https://static.wikia.nocookie.net/thegigaverse/images/6/66/Dark_soulz.jpg/revision/latest/scale-to-width-down/884?cb=20190719081349'
-    # )
-    embed.set_author(name=ctx.author.display_name,
-                     url='https://www.google.com',
-                     icon_url=ctx.author.avatar_url)
+    embed = discord.Embed(
+        title="Total Value",
+        description=f"Total value in gold at level {target_level:,}",
+        url='https://repl.it/@Suntoria/sun-bot#main.py',
+        color=0xFF5733)  #,color=Hex code
+    embed.set_author(
+        name=ctx.author.display_name,
+        url=
+        'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstleyVEVO',
+        icon_url=ctx.author.avatar_url)
     embed.set_thumbnail(
         url=
         'https://static.wikia.nocookie.net/grow-castle/images/1/1f/Gold_Coin.png/revision/latest?cb=20180508232731'
     )
     embed.add_field(name='Hero | Leader | Tower',
-                    value=f'{0}\tgold',
+                    value=f'{upgrade_cost_total( target_level):,}\tgold',
                     inline=False)
     embed.add_field(name='Castle',
-                    value=f'{0}\tgold',
+                    value=f'{upgrade_castle_total( target_level):,}\tgold',
                     inline=False)
     embed.add_field(name='Tower-Archer',
-                    value=f'{0}\tgold',
+                    value=f'{upgrade_TA_total( target_level):,}\tgold',
                     inline=False)
-    # embed.add_field(name="Name",
-    #                 value="you can make as much as fields you like to")
-    # embed.set_footer(name="footer")  #if you like to
+
     await ctx.send(embed=embed)
 
 
@@ -112,7 +120,6 @@ async def on_command_error(ctx, error):
 
 def main():
     keep_alive()
-    # EXECUTES THE bot WITH THE SPECIFIED TOKEN. TOKEN HAS BEEN REMOVED AND USED JUST AS AN EXAMPLE.
     bot.run(os.getenv("DISCORD_TOKEN"))
 
 
